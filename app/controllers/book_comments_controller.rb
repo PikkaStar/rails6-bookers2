eadc71eda67735f5da526d49a0e6d4cd4bd9a62a
PikkaStar <yokotaka1226@gmail.com>
2023-10-17 07:56:36 +0000

    favorite

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @books=Book.find(params[:book_id])
+   @book_comment = BookComment.new
+
   end
 
-  def destroy
-    comment = BookComment.find(params[:id])
-    
-    comment.destroy
-    redirect_back(fallback_location: root_path)
+   def destroy
+  @books=Book.find(params[:book_id])
+  @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+  @book_comment.destroy
+  @book_comment = BookComment.new
   end
 
   private
